3054a1a38c171606d2615b288f8de045a96fc13d
Nir Lahad <nirlah@gmail.com>
2018-12-19 13:01:39 +0200

    Add ID icons

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const uuid = require('short-uuid');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
@@ -7,26 +8,23 @@ server.listen(80);
 app.use(express.static('public'));
 
 const colors = [
-  '#673AB7',
-  '#00BCD4',
-  '#FFC107',
-  '#9C27B0',
-  '#03A9F4',
-  '#8BC34A',
-  '#E91E63',
-  '#2196F3',
-  '#4CAF50',
-  '#F44336',
-  '#3F51B5',
-  '#009688',
-  '#FF5722',
-  '#607D8B'
+  '#673AB7', '#00BCD4', '#FFC107', '#9C27B0', '#03A9F4', '#8BC34A', '#E91E63',
+  '#2196F3', '#4CAF50', '#F44336', '#3F51B5', '#009688', '#FF5722', '#607D8B'
+];
+const icons = [
+  'apple', 'bread', 'bug', 'burger', 'cannon', 'cheese', 'cherry', 'crab', 'dog',
+  'goat', 'gun', 'horse', 'monkey', 'octopus', 'ox', 'pig', 'pineapple', 'pizza',
+  'rabbit', 'rat', 'rifle', 'rooster', 'shrimp', 'snake', 'spider', 'strawberry', 'sword'
 ];
 let colorCounter = 0;
+let iconCounter = 0;
 
 io.on('connection', function (socket) {
-  socket.emit('color', colors[colorCounter % colors.length]);
-  colorCounter++;
+  socket.emit('id', {
+    color: colors[colorCounter++ % colors.length],
+    icon: icons[iconCounter++ % icons.length],
+    tag: uuid.generate()
+  });
 
   socket.on('circle', function (c) {
     socket.broadcast.emit('circle', c);
--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 const LIFESPAN = 60;
 const THIRD_LIFESPAN = Math.floor(LIFESPAN / 3);
+const SHOW_ME = 14000;
+const SHOW_OTHERS = 7000;
 
 const START_RADIUS = 30;
 const BRUSH_RADIUS = 60;
@@ -8,6 +10,7 @@ const text1 = 'אם עץ נופל ביער ואף אחד לא שומע,'.split('
 const text2 = 'האם הוא השמיע צליל?'.split('').reverse().join('');
 
 let font, audio;
+const icons = {}, otherIcons = {};
 let brushes = {};
 let lastDraw = [-1, -1];
 
@@ -17,12 +20,26 @@ let isAudioPlaying = false;
 let shouldStopAudio = false;
 
 let socket;
+let myTag = false;
 let myColor = '#000000';
+let myIcon = 'bug';
 
 function preload() {
   soundFormats('mp3');
   audio = loadSound('assets/typewriter.mp3');
   font = loadFont('assets/EditorSans-Normal.otf');
+
+  [
+    'apple', 'bread', 'bug', 'burger', 'cannon', 'cheese', 'cherry', 'crab', 'dog',
+    'goat', 'gun', 'horse', 'monkey', 'octopus', 'ox', 'pig', 'pineapple', 'pizza',
+    'rabbit', 'rat', 'rifle', 'rooster', 'shrimp', 'snake', 'spider', 'strawberry', 'sword'
+  ]
+    .forEach(icon => {
+      loadStrings(`assets/icons/${icon}.svg`, strs => {
+        const svg = strs.join('').replace('<svg', '<svg fill="#ffffff"').split('#').join('%23');
+        icons[icon] = loadImage('data:image/svg+xml;charset=utf-8,' + svg);
+      });
+    })
 }
 
 function setup() {
@@ -40,15 +57,17 @@ function setup() {
   socket = io.connect(window.location.href);
 
   // On connection
-  socket.on('color', d => {
-    myColor = d;
+  socket.on('id', d => {
+    myTag = d.tag;
+    myIcon = d.icon;
+    myColor = d.color;
   });
 
   // On new particle
   socket.on('circle', d => {
     let brush;
-    brush = brushes[d.color];
-    if (!brush) brush = brushes[d.color] = new Brush(d.color);
+    brush = brushes[d.tag];
+    if (!brush) brush = brushes[d.tag] = new Brush(d.tag, d.color, d.icon);
 
     const x = d.x * window.innerWidth;
     const y = d.y * window.innerHeight;
@@ -62,8 +81,8 @@ function draw() {
   tickInteraction();
 
   drawBrushes();
-  drawCursor();
   drawText();
+  drawCursor();
 }
 
 function tickInteraction() {
@@ -73,6 +92,8 @@ function tickInteraction() {
       socket.emit('circle', {
         x: mouseX / window.innerWidth,
         y: mouseY / window.innerHeight,
+        icon: myIcon,
+        tag: myTag,
         color: myColor
       });
 
@@ -104,14 +125,20 @@ function drawBrushes() {
   bs.forEach(b => b.tick());
   const shoudlKill = bs.filter(b => b.shoudlKill);
   shoudlKill.forEach(b => {
-    delete brushes[b.color];
+    delete brushes[b.tag];
   })
 }
 
 function drawCursor() {
-  fill(myColor);
   noStroke();
-  ellipse(mouseX, mouseY, 3);
+  fill(myColor);
+
+  if (millis() > SHOW_ME) {
+    ellipse(mouseX, mouseY, 20);
+    image(icons[myIcon], mouseX - 6, mouseY - 6, 12, 12);
+  } else {
+    ellipse(mouseX, mouseY, 3);
+  }
 }
 
 function drawText() {
@@ -146,9 +173,18 @@ function drawTextLayer(bx, by, l) {
 
 class Brush {
 
-  constructor(color) {
+  constructor(tag, color, icon) {
+    this.tag = tag;
     this.color = color;
+    this.icon = icon;
     this.particles = [];
+    this.lastX = 0;
+    this.lastY = 0;
+
+    loadStrings(`assets/icons/${icon}.svg`, strs => {
+      const svg = strs.join('').replace('<svg', `<svg fill="${color}"`).split('#').join('%23');
+      otherIcons[color + icon] = loadImage('data:image/svg+xml;charset=utf-8,' + svg);
+    });
   }
 
   get shoudlKill() {
@@ -156,6 +192,8 @@ class Brush {
   }
 
   add(x, y) {
+    this.lastX = x;
+    this.lastY = y;
     this.particles.push(new Particle(x, y));
     this.particles.push(new Particle(x, y));
     this.particles.push(new Particle(x, y));
@@ -174,6 +212,14 @@ class Brush {
 
     // Draw Verts
     this.drawVerts();
+
+    // Draw icon
+    if (millis() > SHOW_OTHERS) {
+      noStroke();
+      fill(255);
+      ellipse(this.lastX, this.lastY, 20);
+      image(otherIcons[this.color + this.icon], this.lastX - 6, this.lastY - 6, 12, 12);
+    }
   }
 
   drawVerts() {
