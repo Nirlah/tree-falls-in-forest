17d31ecbbafc552ee997ff3fbd16d027c9c10bf2
Nir Lahad <nirlah@gmail.com>
2018-12-21 10:14:00 +0200

    Instructions

---
+++ README.md
@@ -0,0 +1,11 @@
+### Install
+Requires Node & NPM to be installed on the system.
+```
+> npm install
+```
+
+### Run
+```
+> node ./
+```
+Site should be live at `localhost:3000` and can be open in multiple browser windows.
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(3000);
 app.use(express.static('public'));
 
 const colors = [
